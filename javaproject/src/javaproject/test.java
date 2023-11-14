448bed99737e22cc34a9db7d1de2a2c07b96c50f
주형 <143166461+cutegay123@users.noreply.github.com>
2023-11-14 11:43:14 +0900

    Add files via upload

---
+++ javaproject/src/javaproject/test.java
@@ -0,0 +1,10 @@
+package javaproject;
+
+public class test {
+
+	public static void main(String[] args) {
+		String message = "날짜_서버프로그램 구현 : 허주형";
+        System.out.println(message);
+	}
+
+}
---
+++ javaproject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author GR
+ *
+ */
+module javaproject {
+}
